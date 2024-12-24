c577815ed0edd4be01c458175c60a2f9467b0597
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2024-12-24 17:53:37 +0700

    Initial project using https://start.spring.io/

---
+++ src/main/java/org/sorasolution/backend/SoraBackendApplication.java
@@ -0,0 +1,12 @@
+package org.sorasolution.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoraBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SoraBackendApplication.class, args);
+	}
+}
